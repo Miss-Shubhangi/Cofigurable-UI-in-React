08136ff4e799e3830e60b47f12922d6b29e36467
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-06-22 20:13:51 +0530

    configData filr created and shop logo added in img folder .

---
+++ src/configData.js
@@ -0,0 +1,19 @@
+import Logo from "./Imgs/logo1.png";
+
+const title= " Apna Cake shop ";
+const ShopImg = Logo;
+
+const Features=["A good Oven" , "Freshness"," Customer Service" ,
+    "Affordable prices","Discounts and Coupons", "Quality"
+];
+
+const Flavors= ['Vanilla', 'Chocolate', 'Strawberry', 'Red Velvet', 'Lemon'];
+const theme= {
+        primaryColor: "#EAAA85",
+        secondaryColor: "#F5E0D2 ",
+        Heading: "#232323",
+        description:"#616161"
+        
+    }
+
+export {title,ShopImg,Features,Flavors,theme}
