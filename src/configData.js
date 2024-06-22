f8d1adebcaf9da3c9838051f4845208bd0c2d7bc
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-06-22 21:50:48 +0530

    Feature images added .

--- src/configData.js
+++ src/configData.js
@@ -1,4 +1,15 @@
 import Logo from "./Imgs/logo1.png";
+import Vanilla from "./Imgs/vanilla.jpg"
+import Chocolate from "./Imgs/chocolate.jpg"
+import Srawberry from "./Imgs/strawberry.jpg"
+import RedVelvet from "./Imgs/redVelvet.jpg"
+import Lemon from "./Imgs/lemon.jpg"
+import Oven from "./Imgs/oven.png"
+import Freshness from "./Imgs/fresh.png"
+import CustomerService from "./Imgs/24-7.png"
+import Affordable from "./Imgs/low-cost.png"
+import Discount from "./Imgs/coupon.png"
+import Quality from "./Imgs/high-quality.png"
 
 const ShopDeatails={
  ShopName: " Apna Cake shop ",
@@ -6,18 +17,55 @@ const ShopDeatails={
  TagLine:"Love At First Bite."
 }
 
-
-const Features=["A good Oven" , "Freshness"," Customer Service" ,
-    "Affordable prices","Discounts and Coupons", "Quality"
+const Features=[{
+    featureName: "A good Oven",
+    featureImg:Oven
+},
+{
+    featureName:"Freshness" ,
+    featureImg:Freshness
+},
+{
+    featureName:" Customer Service" ,
+    featureImg:CustomerService
+},
+{
+    featureName:"Affordable prices" ,
+    featureImg:Affordable
+},
+{
+    featureName:"Discounts and Coupons" ,
+    featureImg:Discount
+}
+{
+    featureName:"Quality" ,
+    featureImg:Quality
+}
 ];
+const Flavors= [
+    {
+    flavourName:'Vanilla',
+    flavourImg:Vanilla},
+   {
+    flavourName:'Chocolate',
+    flavourImg:Chocolate},
+   {
+    flavourName:'Strawberry',
+    flavourImg:Srawberry},
+   {
+    flavourName:'Red Velvet',
+    flavourImg:RedVelvet},
+   {
+    flavourName:'Lemon',
+    flavourImg:Lemon}
 
-const Flavors= ['Vanilla', 'Chocolate', 'Strawberry', 'Red Velvet', 'Lemon'];
+
+ ];
 const theme= {
-        primaryColor: "#EAAA85",
-        secondaryColor: "#F5E0D2 ",
-        Heading: "#232323",
-        description:"#616161"
-
-    }
+    primaryColor: '#ff6347', // Tomato
+    secondaryColor: '#ffa500', // Orange
+    headingColor: '#4b0082', // Indigo
+    subheadingColor: '#20b2aa', // Light Sea Green
+  }
 
 export {ShopDeatails,Features,Flavors,theme}
